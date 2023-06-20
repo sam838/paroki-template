e56d58a7c45aa9721954644385ec4af6decd6887
samuel <napogistra@gmail.com>
2023-06-20 15:13:24 +0700

    Coba home

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,5 @@
 <template>
-  <div>
-    <AboutTwo />
-    <section class="course-one__top-title home-one">
-      <div class="container2">
-        <div class="block-title mb-0">
-          <h2 class="block-title__title">
-            Artikel<br>
-            Terkini
-          </h2><!-- /.block-title__title -->
-        </div><!-- /.block-title -->
-      </div><!-- /.container -->
-      <div class="course-one__top-title__curve" /><!-- /.course-one__top-title__curve -->
-    </section>
-
-    <section class="news-section mt-16 container-2">
-      <div class="d-flex flex-column mx-auto card-container py-5 mt-6">
-        <div class="d-flex flex-column mx-auto card-container">
-          <IndexMain
-            v-for="(item, i) in newsMain"
-            :key="i"
-            :item="item"
-          />
-        </div>
-        <div class="d-flex justify-space-between">
-          <v-col
-            v-for="(item, i) in newsContent"
-            :key="i"
-          >
-            <IndexCard2
-              :item="item"
-              class="dflex mx-auto"
-            />
-          </v-col>
-        </div>
-      </div>
-    </section>
-  </div>
+  <Home />
 </template>
 <script>
 export default {
